50e239ffe7f4a201643731879b195c814fa14ab5
wannabesuicider@gmail.com <wannabesuicider@gmail.com>
2021-06-12 09:31:36 +0300

    cliaws15

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+ReactDOM.render(<h1>Hi there!</h1>, document.querySelector('#rooter'));
 ReactDOM.render(<App />, document.querySelector('#root'));
